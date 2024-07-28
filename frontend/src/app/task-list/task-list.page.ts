6c98a7d58fe31316b562bf19dc57149b8b52076e
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-07-29 02:57:53 +0900

    update & delete

--- frontend/src/app/task-list/task-list.page.ts
+++ frontend/src/app/task-list/task-list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActionSheetController, AlertController } from "@ionic/angular";
 
 @Component({
   selector: 'app-task-list',
@@ -11,12 +12,71 @@ export class TaskListPage {
     { name: 'タスク1' },
     { name: 'タスク2' },
   ];
-  constructor() { }
+  constructor(
+    public actionSheetController: ActionSheetController,
+    public alertController: AlertController,
+  ) { }
 
   ionViewWillEnter() {
     const storedTasks = localStorage.getItem('tasks');
     if (storedTasks) {
       this.tasks = JSON.parse(storedTasks);
     }
+  }
+
+  async changeTask(index: number) {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'タスクの変更',
+      buttons: [
+        {
+          text: '削除',
+          role: 'destructive',
+          icon: 'trash',
+          handler: () => {
+            this.tasks.splice(index, 1);
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+          }
+        }, {
+          text: '変更',
+          icon: 'create',
+          handler: () => {
+            this._renameTask(index);
+          }
+        }, {
+          text: '閉じる',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+      ]
+    });
+    await actionSheet.present();
+  }
+
+  async _renameTask(index: number) {
+    const prompt = await this.alertController.create({
+      header: '変更後のタスク',
+      inputs: [
+        {
+          name: 'task',
+          placeholder: 'タスク',
+          value: this.tasks[index].name,
+        },
+      ],
+      buttons: [
+        {
+          text: '閉じる'
+        },
+        {
+          text: '保存',
+          handler: data => {
+            this.tasks[index] = { name: data.task };
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+          }
+        }
+      ]
+    });
+    await prompt.present();
   }
 }
