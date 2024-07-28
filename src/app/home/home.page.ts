eabc104f142b481ec9d2b2e2f0aa3caca96f6dc2
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-07-28 21:43:24 +0900

    add Home

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,16 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+})
+export class HomePage implements OnInit {
+  public folder!: string;
+  private activatedRoute = inject(ActivatedRoute);
+  constructor() {}
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+}
